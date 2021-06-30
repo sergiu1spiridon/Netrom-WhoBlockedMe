584a511ef1922231d87a863bfa153c5eb15ce637
sergiu1spiridon <40805498+sergiu1spiridon@users.noreply.github.com>
2021-06-30 11:17:43 +0300

    made entities

---
+++ src/Entity/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="String", length = 255)
+     */
+    private $emailAddress;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmailAddress()
+    {
+        return $this->emailAddress;
+    }
+
+    /**
+     * @param mixed $emailAddress
+     */
+    public function setEmailAddress($emailAddress): void
+    {
+        $this->emailAddress = $emailAddress;
+    }
+
+
+}
---
+++ src/Repository/LicencePlateRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\LicencePlate;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method LicencePlate|null find($id, $lockMode = null, $lockVersion = null)
+ * @method LicencePlate|null findOneBy(array $criteria, array $orderBy = null)
+ * @method LicencePlate[]    findAll()
+ * @method LicencePlate[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LicencePlateRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, LicencePlate::class);
+    }
+
+    // /**
+    //  * @return LicencePlate[] Returns an array of LicencePlate objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?LicencePlate
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
